b953b936d5b06dfc1d8581aee7212d0d1ecc87a1
Carnelas <carnelas@gmail.com>
2018-11-07 20:22:11 +0100

    queue todavia

--- solution_exercise5/src/queue/queue.js
+++ solution_exercise5/src/queue/queue.js
@@ -1,23 +1,26 @@
 const kue = require('kue');
-let jobs = kue.createQueue();
+let queue = kue.createQueue();
 let util = require('util');
 const sendMessage = require("../controllers/sendMessage");
 
 module.exports = function (req, res) {
-    let job = jobs.create("msg", {
+    // the first argument identifies the type of job, and any additional 
+    // data is passed as a hash via the second argument.
+    let job = queue.create("msg", {
         destination: util.inspect(req.body),
         body: util.inspect(req.body)
-    })
-        .save(function (err) {
-            if (!err) res.sendStatus(200).send(job.id);
-        });
+    }).save(function (err) {
+        if (!err) res.sendStatus(200).send(job.id);
+    });
 };
 
-jobs.on("job enqueue", function (id, type) {
+queue.on("job enqueue", function (id, type) {
     console.log("Job %s got queued of type %s", id, type);
 });
-jobs.save();
+queue.save();
 
-jobs.process('new job', function (job, done) {
+// The first argument should be the name of the job, and the second a function, 
+// which provides as arguments the job itself and a callback.
+queue.process('msg', function (job, done) {
     sendMessage(job.data.body, done)
 });
